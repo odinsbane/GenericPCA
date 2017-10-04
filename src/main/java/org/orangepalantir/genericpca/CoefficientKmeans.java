a0a20770f6c7ebc74ffb85eeb258774602bdb1f2
msmith <msmith@pki-cbg.de>
2017-10-04 08:25:19 +0100

    Started a new class, for calculating kmeans with coefficients and separating into populations.

---
+++ src/main/java/org/orangepalantir/genericpca/CoefficientKmeans.java
@@ -0,0 +1,16 @@
+package org.orangepalantir.genericpca;
+
+import java.util.List;
+
+/**
+ * Created by msmith on 04.10.17.
+ */
+public class CoefficientKmeans {
+    List<List<IndexedCoefficient>> coefficients;
+    public void setInput(List<List<IndexedCoefficient>> input){
+        coefficients = input;
+    }
+    public static void main(String[] args){
+
+    }
+}
